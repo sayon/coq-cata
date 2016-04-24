39662a1d6de4c1fdfcdcbee21ccc3b4b74414a15
sayon <igorjirkov@gmail.com>
2016-04-24 12:35:53 +0200

    flush

--- Cata.v
+++ Cata.v
@@ -2,48 +2,36 @@ From mathcomp.ssreflect Require Import ssreflect eqtype.
 Require Import Common Functor.
 Require Import Coq.Logic.FunctionalExtensionality.
 
-Inductive expr a :=
-| add : a -> a -> expr a
-| lit: a -> expr a.
-(*
-
-
-Axiom atfix: exists t, expr t = t.
-
-Fixpoint algebra (e: expr nat) : nat :=
-  match e with
-    | add  x y => x + y
-    | lit x => x
-  end.*)
+Inductive exprf a :=
+| add : a -> a -> exprf a
+| lit: nat -> exprf a.
 
 
 Definition Algebra (F:functor) (a:Type)  := F a -> a.
 Definition CoAlgebra (F:functor) (a:Type)  := F a -> a.
 
-Definition expr_fmap {A B} (f: A->B) (e: expr A) :expr B :=
+Definition exprf_fmap {A B} (f: A->B) (e: exprf A) :exprf B :=
   match e with
                  | add x x0 => add _ (f x) (f x0)
-                 | lit x => lit _ $ f x
+                 | lit x => lit _ x
   end.
 
-Definition functor_expr_mixin : Functor.mixin_of expr.
-  refine ( FunctorMixin expr (@expr_fmap) _ _ ).
-  done.
-  move=> A B C f g.
-  extensionality e.
-  elim: e f g; by intros.
-Defined.
+Definition functor_exprf_mixin : Functor.mixin_of exprf.  refine ( FunctorMixin exprf (@exprf_fmap) _ _ ).  done.  move=> A B C f g.  extensionality e.  elim: e f g; by intros.  Defined.
 
-Canonical ExprFunctor := Functor expr functor_expr_mixin.
+Canonical ExprFFunctor := Functor exprf functor_exprf_mixin.
 
-Definition eval: Algebra expr.
+Definition eval: Algebra ExprFFunctor nat := 
+  fun e=> match e with
+            | add x y => x + y
+            | lit x => x
+          end.
+
+(* I can't think of another way of expressing fixpoint *)
+
+Inductive expr := In { Out: exprf expr }.
+Definition Expr := In.
+Definition ex := Expr ( add _ (Expr $ lit _ 4) (Expr $ lit _ 8)).
+
+Definition expr_cata {T} (e: expr) (alg:Algebra ExprFFunctor T) : T :=
   
-
-               
   
-
-CoFixpoint tfix {a: Type} (t:a->expr a) : a := t (tfix t).
-
-                               | 
-CoInductive iterate (f:Type->Type): 
-Fixpoint Mu (f:forall A: A->A) := f (Mu f).
